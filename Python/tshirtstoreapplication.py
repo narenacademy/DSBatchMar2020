0145b209f208dede367f43572969d78c268193ed
Roshan Sharma <61927837+resuun@users.noreply.github.com>
2020-03-08 11:21:28 +0530

    Update tshirtstoreapplication.py
    final commit

--- Python/tshirtstoreapplication.py
+++ Python/tshirtstoreapplication.py
@@ -1,16 +1,12 @@
-<<<<<<< HEAD
-takeorder()
-print "hello"
-print'hiiii'
-=======
+
 def validate(size,color,cloth):
     if((size=='S' or size== 'M' or size == 'L') and (color=='R' or color== 'G' or color== 'B') and (cloth=='C' or cloth== 'S')):
         placeorder(size,color,cloth,'True')
     else:
         placeorder(size,color,cloth,'False')
->>>>>>> 31c80fa8d15ba4bea244434340f2a49e9504a66d
+
 def placeorder(size,color,cloth,orderstatus):
-if (orderstatus == true)
+if (orderstatus == 'True')
     print('your order is placed with size or color or clothe type:',size,color,cloth)
-else
+else:
     print('your order is not placed as its out of stock')
