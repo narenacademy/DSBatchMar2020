6690afd39111b44a6070f4467c17f7a43a76d001
sarika1111 <61927874+sarika1111@users.noreply.github.com>
2020-03-08 23:46:33 +0530

    Update tshirtstoreapplication.py
    my method printing

--- Python/tshirtstoreapplication.py
+++ Python/tshirtstoreapplication.py
@@ -9,3 +9,8 @@ def validate(size,color,cloth):
     else:
         placeorder(size,color,cloth,'False')
 >>>>>>> 31c80fa8d15ba4bea244434340f2a49e9504a66d
+def placeorder(size,color,cloth,orderstatus):
+if (orderstatus == true)
+    print('your order is placed with size or color or clothe type:',size,color,cloth)
+else
+    print('your order is not placed as its out of stock')
