31c80fa8d15ba4bea244434340f2a49e9504a66d
Roshan Sharma <61927837+resuun@users.noreply.github.com>
2020-03-08 11:08:58 +0530

    Update tshirtstoreapplication.py
    my method pushed

--- Python/tshirtstoreapplication.py
+++ Python/tshirtstoreapplication.py
@@ -1,1 +1,5 @@
-takeorder()
+def validate(size,color,cloth):
+    if((size=='S' or size== 'M' or size == 'L') and (color=='R' or color== 'G' or color== 'B') and (cloth=='C' or cloth== 'S')):
+        placeorder(size,color,cloth,'True')
+    else:
+        placeorder(size,color,cloth,'False')
