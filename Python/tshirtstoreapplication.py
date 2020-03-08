55e5855b256abde058a651ca63e912ec4f195302
Benhur433 <61927847+Benhur433@users.noreply.github.com>
2020-03-08 11:23:18 +0530

    Update tshirtstoreapplication.py
    added input

--- Python/tshirtstoreapplication.py
+++ Python/tshirtstoreapplication.py
@@ -3,6 +3,16 @@ takeorder()
 print "hello"
 print'hiiii'
 =======
+def takeorder():
+    size=input('Enter size as S,M,L')
+    colour=input ('Enter Colour as R,G,B')
+    cloth=input ('Enter Cloth as C,S')
+    validate(size,color,cloth)
+def takeorder():
+    size=input('Enter size as S,M,L')
+    colour=input ('Enter Colour as R,G,B')
+    cloth=input ('Enter Cloth as C,S')
+    validate(size,color,cloth)
 def validate(size,color,cloth):
     if((size=='S' or size== 'M' or size == 'L') and (color=='R' or color== 'G' or color== 'B') and (cloth=='C' or cloth== 'S')):
         placeorder(size,color,cloth,'True')
